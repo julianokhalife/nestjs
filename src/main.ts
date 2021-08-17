e75f3a6f6b191fa6ef7fcf3d317073867526e31d
Juliano Khalife <khalifejuliano2@gmail.com>
2021-08-17 21:19:23 +0300

    chore: Load database config from .env file chore: Added config validation

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
-import { Logger } from "@nestjs/common";
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger();
